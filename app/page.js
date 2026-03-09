f3f007dc10e9cf75479b45a60226cc26c3240e3d
Rushal <devilrushh777@gmail.com>
2026-03-09 21:56:24 +0530

    completed sign-in and sign-up UI

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>  
+      <h2> welcome</h2>
+      <Button> hey
+      
+      </Button>
+
+
+
+    </div>
+   
+  );
+}
