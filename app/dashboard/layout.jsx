3476880c1b36f0f7cfa7d4fdf8d1b125b45d3870
Rushal <devilrushh777@gmail.com>
2026-03-10 18:31:11 +0530

    installed drizzle kit

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,9 +1,11 @@
 
 import React from 'react'
+import Header from './__components/Header';
 
 function DashboardLayout({children}) {
    return (
     <div>
+      <Header/>
       {children}
     </div>
   );
